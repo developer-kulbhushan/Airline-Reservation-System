168891f179b75229bb2714aa990acc1edb36485f
Kulbhushan <officialkulbhushan@gmail.com>
2021-01-07 14:23:12 +0530

    Added run.bat file

---
+++ run.bat
@@ -0,0 +1,8 @@
+set CLASSPATH=%CLASSPATH%;C:\Program Files\Java\jre1.8\rt.jar;
+set CLASSPATH=%CLASSPATH%;%cd%/External Libraries/flatlaf-demo-0.44.jar;
+set CLASSPATH=%CLASSPATH%;%cd%/External Libraries/dom-2.3.0-jaxb-1.0.6.jar;
+set CLASSPATH=%CLASSPATH%;%cd%/External Libraries/jdatepicker-1.3.4.jar;
+set CLASSPATH=%CLASSPATH%;%cd%/External Libraries/mysql-connector-java-8.0.21.jar;
+cls
+java home.Run
+PAUSE
